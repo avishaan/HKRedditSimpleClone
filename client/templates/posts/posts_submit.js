57b1aac0da95dfcd7dd8d52d541c6ed39f6ed876
Avishaan <avishaan@codeHatcher.com>
2015-08-09 02:45:35 -0700

    add post attributes such as userid and also check for duplicate urls

--- client/templates/posts/posts_submit.js
+++ client/templates/posts/posts_submit.js
@@ -9,5 +9,17 @@ Template.postSubmit.events({
 
     post._id = Posts.insert(post);
     Router.go('postPage', post);
+    Meteor.call('postInsert', post, function(err, results) {
+      // display error to user and abort
+      if (err)
+        return alert(err.reason);
+
+      // show this result but route anyway
+      if (result.postExists)
+        alert('This link has laready been posted');
+
+      Router.go('postPage', {_id: result._id});
+
+    });
   }
 });
