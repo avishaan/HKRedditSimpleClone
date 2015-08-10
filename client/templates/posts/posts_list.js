4cb6a0038675093519ffa92a966d9893191e1e3f
Avishaan <avishaan@codeHatcher.com>
2015-08-10 16:42:48 -0700

    pagination on postsPage route

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,5 +1,7 @@
-Template.postsList.helpers({
-  posts: function() {
-    return Posts.find({}, { sort: { submitted: -1 }});
-  }
-});
+// we got rid of this because in our router.js we are returning our data explicitly as post
+// https://book.discovermeteor.com/chapter/pagination
+// Template.postsList.helpers({
+//   posts: function() {
+//     return Posts.find({}, { sort: { submitted: -1 }});
+//   }
+// });
