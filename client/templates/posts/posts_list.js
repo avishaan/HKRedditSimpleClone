c43bfe30d481bbc314ab19b46831101fabea7494
Avishaan <avishaan@codeHatcher.com>
2015-08-11 17:08:56 -0700

    add animation when posts need to switch order

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -5,3 +5,43 @@
 //     return Posts.find({}, { sort: { submitted: -1 }});
 //   }
 // });
+
+Template.postsList.onRendered(function() {
+  this.find('.wrapper')._uihooks = {
+    moveElement: function(node, next) {
+      var $node = $(node); //element being moved to new dom position
+      var $next = $(next); //element right after the new position node is being moved to
+      var oldTop = $node.offset().top;
+      var height = $node.outerHeight(true);
+
+      // find all elements between next and node
+      var $inBetween = $next.nextUntil(node);
+      if ($inBetween.length === 0)
+        $inBetween = $node.nextUntil(next);
+
+      // put node in correct place
+      $node.insertBefore(next);
+
+      // measure new top
+      var newTop = $node.offset().top;
+
+      // move node "back" to where it was before
+      $node
+      .removeClass('animate')
+      .css('top', oldTop - newTop);
+
+      // push every other element down (or up) to put them back
+      $inBetween
+      .removeClass('animate')
+      .css('top', oldTop < newTop ? height : -1 * height);
+
+      // force a redraw
+      $node.offset()
+
+      // reset everything to 0, animated
+      $node.addClass('animate').css('top', 0);
+      $inBetween.addClass('animate').css('top', 0);
+
+    }
+  }
+});
